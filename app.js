b5326aa35c7308bee29a003f0839d1b527f59a9d
Ji Won Chang <henry0205c@gmail.com>
2018-11-07 02:56:24 +0000

    Second commit, connect to DB and add routes. Fun!

--- app.js
+++ app.js
@@ -1,29 +1,80 @@
-var express = require("express"),
-    app     = express();
+// require installation
+var methodOverride   = require("method-override"),
+    bodyParser       = require("body-parser"),
+    mongoose         = require("mongoose"),
+    express          = require("express"),
+    app              = express();
     
+// require models from modules
+var Blog = require("./models/blogs");
+    
+// general set-up
 app.set("view engine", "ejs");    
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
+// connects our express app to the mongoDB
+mongoose.connect("mongodb://localhost/p_web", {useNewUrlParser: true});
+
+// index page routes ************************
 app.get("/", function(req, res) {
     res.render("index");
 });
 
-app.get("/about", function(req, res) {
-    res.render("about");
-});
+// app.get("/about", function(req, res) {
+//     res.render("about");
+// });
 
+// resume routes ****************************
 app.get("/resume", function(req, res) {
     res.render("resume");
 });
 
+// portfolio routes *************************
 app.get("/portfolio", function(req, res) {
     res.render("portfolio");
 });
 
+// blog routes ******************************
+ // INDEX blog route
 app.get("/blog", function(req, res) {
-    res.render("blog");
+    Blog.find({}, function(err, allBlogs) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.render("blog", {blogs: allBlogs});
+        }
+    });
+});
+ // NEW blog route
+app.get("/blog/new", function(req, res) {
+    res.render("blogs/new");
+});
+ // CREATE blog route
+app.post("/blog", function(req, res) {
+    var newBlog = req.body.blog;
+    Blog.create(newBlog, function(err, newlyCreated) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/blog");
+        }
+    });
+});
+ // SHOW blog route
+app.get("/blog/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blog");
+            console.log(err);
+        } else {
+            res.render("blogs/show", {blog: foundBlog});
+        }
+    });
 });
 
+// contact me routes ************************
 app.get("/contact", function(req, res) {
     res.render("contact");
 });
---
+++ models/blogs.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+// blog schema
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: {type: String, default: "https://i.kinja-img.com/gawker-media/image/upload/s--gNscB14c--/c_scale,f_auto,fl_progressive,q_80,w_800/18j2u6zfdsj9bjpg.jpg"},
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+// blog model
+var Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
+
+  // creates a blog object and saves it into our database
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1538523396059-8145b69118c4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a33b59b20f6b6efcd88bdd73b1cb84c2&auto=format&fit=crop&w=701&q=80",
+//     body: "THIS IS A TEST BLOG!"
+// });
