823d9478295ecaf88f688f0255381b3b23c19547
Ji Won Chang <henry0205c@gmail.com>
2018-12-04 22:57:13 +0000

    Fourteenth Commit. Add email functionality to 'Contact Me' page. Minor changes.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var expressSanitizer = require("express-sanitizer"),
     methodOverride   = require("method-override"),
     bodyParser       = require("body-parser"),
+    nodemailer       = require("nodemailer"),
     mongoose         = require("mongoose"),
     express          = require("express"),
     app              = express();
@@ -217,6 +218,55 @@ app.delete("/blog/:id", function(req, res) {
 app.get("/contact", function(req, res) {
     res.render("contact");
 });
+
+app.post("/contact", function(req, res) {
+    const output = `
+        <p>You have a new contact request:</p>
+        <h3>Contact Details:</h3>
+        <ul>
+            <li>Name: ${req.body.contact.name}</li>
+            <li>email: ${req.body.contact.email}</li>
+        </ul>
+        <h3>Message:</h3>
+        <p>${req.body.contact.message}</p>
+    `;
+    
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env.GMAIL_USER, // environment variable
+            pass: process.env.GMAIL_PASS // generated ethereal password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: `'"${req.body.contact.name}" <${req.body.contact.email}>'`, // sender address
+        to: process.env.GMAIL_RECEIVER, // list of receivers
+        subject: req.body.contact.subject, // Subject line
+        // text: `${req.body.contact.name} (${req.body.contact.email}) says: ${req.body.contact.message}` // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            res.redirect("/");
+        }
+    });
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Server Has Launched!");
